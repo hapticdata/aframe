3d4061eb2b7db3c8c03cb36bbd68b85eb198433d
Kyle Phillips <kylephillips@google.com>
2017-08-01 13:24:01 -0400

    new dist for onVRPresentChange fix

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.6.1 (Date 01-08-2017, Commit #4f9d0fb)');
+console.log('A-Frame Version: 0.6.1 (Date 01-08-2017, Commit #9e1af5ad)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
